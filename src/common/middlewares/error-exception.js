af683bd34f55048411391d7f9d4e632d3bbe799c
dsad233 <ksl1421@naver.com>
2026-02-13 22:45:47 +0900

    fix: 에러 반환 방식 변경

--- src/common/middlewares/error-exception.js
+++ src/common/middlewares/error-exception.js
@@ -2,5 +2,5 @@ import { StatusCodes } from 'http-status-codes';
 export const ErrorHandlerMiddleware = function (err, req, res, next) {
   return res
     .status(err.StatusCodes || StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ message: err.message });
+    .json({ message: err.message || err });
 };
